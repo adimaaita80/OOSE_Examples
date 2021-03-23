a1d4e130a8d5d5e83ad1650c5dda14efdcb45c4b
Adi Maaita <adi.maaita80@gmail.com>
2021-03-23 08:42:34 +0200

    Add project files.

---
+++ OOSE_Examples/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOSE_Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Supermarket supermarket1 = new Supermarket();
+            Supermarket supermarket2 = new Supermarket();
+
+            supermarket1.SupermarketId = 1;
+            supermarket1.SupermarketName = "Family Supermarket";
+            supermarket1.SupermarketAddress = "Amman-Jordan";
+
+            Console.WriteLine(supermarket1.SupermarketInformation());
+
+
+            supermarket2.SupermarketId = 2;
+            supermarket2.SupermarketName = "Amman Supermarket";
+            supermarket2.SupermarketAddress = "Amman-Jordan";
+
+            Console.WriteLine(supermarket2.SupermarketInformation());
+        }
+    }
+}
---
+++ OOSE_Examples/Supermarket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOSE_Examples
+{
+    public class Supermarket
+    {
+        public int SupermarketId { get; set; }
+        public string SupermarketName { get; set; }
+        public string SupermarketOwnerName { get; set; }
+        public string SupermarketAddress { get; set; }
+        public string SupermarketPhone { get; set; }
+
+        public string SupermarketInformation()
+        {
+            return $"Supermarket name: {SupermarketName} \n Supermarket address: {SupermarketAddress}";
+        }
+    }
+}
